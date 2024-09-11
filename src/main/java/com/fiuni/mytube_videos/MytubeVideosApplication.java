5fac61b6954b6ddb696bf059788b56e01cf12a24
Rodrigo Maidana <114775631+romaidana29@users.noreply.github.com>
2024-09-11 15:15:26 -0400

    Initial commit

---
+++ src/main/java/com/fiuni/mytube_videos/MytubeVideosApplication.java
@@ -0,0 +1,13 @@
+package com.fiuni.mytube_videos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytubeVideosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MytubeVideosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fiuni/mytube_videos/MytubeVideosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiuni.mytube_videos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytubeVideosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
