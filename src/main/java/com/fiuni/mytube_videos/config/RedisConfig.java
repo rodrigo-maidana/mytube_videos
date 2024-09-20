23d35026ffcf4ec7ba218c3776470ebde7f60607
rodrigo-maidana <ro-maidana@hotmail.com>
2024-09-20 16:50:59 -0400

    RedisCOnfig

---
+++ src/main/java/com/fiuni/mytube_videos/config/RedisConfig.java
@@ -0,0 +1,39 @@
+package com.fiuni.mytube_videos.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.cache.RedisCacheManager;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableCaching
+public class RedisConfig {
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        // Configuración por defecto con TTL global de 60 segundos
+        RedisCacheConfiguration defaultCacheConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofDays(365))
+                .disableCachingNullValues();
+
+        // TTL de 10 minutos para el cache mytube_videos
+        RedisCacheConfiguration videoCacheConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(10))
+                .disableCachingNullValues();
+
+        // Mapa para definir las configuraciones de cada cache
+        Map<String, RedisCacheConfiguration> cacheConfigurations = new HashMap<>();
+        cacheConfigurations.put("mytube_videos", videoCacheConfig);
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(defaultCacheConfig)
+                .withInitialCacheConfigurations(cacheConfigurations)
+                .build();
+    }
+}
