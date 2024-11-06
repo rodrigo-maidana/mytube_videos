4b65bee00b7d78036c66c439b1c63fc574aeafb3
rodrigo-maidana <ro-maidana@hotmail.com>
2024-11-06 15:48:04 -0300

    change port cors

--- src/main/java/com/fiuni/mytube_videos/config/WebConfig.java
+++ src/main/java/com/fiuni/mytube_videos/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("localhost:5173")
+                .allowedOriginPatterns("http://localhost:5173/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
