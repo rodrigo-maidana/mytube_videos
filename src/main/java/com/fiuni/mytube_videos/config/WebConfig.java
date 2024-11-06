9d1050851f8a8ccf303e6df0ece725c0e7a42f5a
Rodrigo Maidana <114775631+romaidana29@users.noreply.github.com>
2024-11-06 18:50:36 -0300

    changes

--- src/main/java/com/fiuni/mytube_videos/config/WebConfig.java
+++ src/main/java/com/fiuni/mytube_videos/config/WebConfig.java
@@ -7,14 +7,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @SuppressWarnings("null")
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:5173/")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(false);
     }
 
 }
