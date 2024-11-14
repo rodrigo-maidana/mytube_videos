24173faaf6eb36ce3dbf8b6e7db768dce05a2fb2
rodrigo-maidana <ro-maidana@hotmail.com>
2024-11-14 19:48:53 -0300

    working

--- src/main/java/com/fiuni/mytube_videos/MytubeVideosApplication.java
+++ src/main/java/com/fiuni/mytube_videos/MytubeVideosApplication.java
@@ -3,9 +3,10 @@ package com.fiuni.mytube_videos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @EntityScan(basePackages = "com.fiuni.mytube.domain")
 @EnableCaching
 public class MytubeVideosApplication {
